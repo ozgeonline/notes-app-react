bcc00660f4ff98e4deb948c1419c1f42a3a34142
ozgeonline <nisroc.414@hotmail.com>
2023-09-01 16:04:21 +0300

    added debouncing-2

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,18 @@ import React from "react"
 import Sidebar from "./components/Sidebar"
 import Editor from "./components/Editor"
 import Split from "react-split"
-// import {nanoid} from "nanoid"
-import { onSnapshot, addDoc, doc, deleteDoc, setDoc } from "firebase/firestore" //collection
+import { onSnapshot, addDoc, doc, deleteDoc, setDoc } from "firebase/firestore"
+// onSnapshot(): Bu yöntem, Firestore'daki bir belge veya koleksiyonda yapılan değişiklikleri dinler. **collection:firebase.js
+// addDoc(): Bu yöntem, Firestore'daki bir koleksiyona yeni bir belge ekler.
+// doc(): Bu yöntem Firestore'daki belgeyi referans alır.
+// deleteDoc(): belgeyi Firestore'dan siler.
+// setDoc(): Firestore'daki bir belgeyi günceller.
 import { notesCollection,db } from "./firebase"
 
 export default function App() {
     const [notes, setNotes] = React.useState([])
-        //() => JSON.parse(localStorage.getItem("notes")) || []
-        //setNotes,notların durumunu güncellemek için kullanılır.yeni bir not dizisini alır ve bunu localStorage'a kaydeder.
-        // yerel depodan notları almak için kullanılır. Yerel depoda not yok ise, işlev boş bir dizi döndürür. Aksi takdirde, yerel depoda depolanan notları döndürür.
 
-    const [currentNoteId, setCurrentNoteId] = React.useState("") //(notes[0] && notes[0].id) -- (notes[0]?.id) || ""
+    const [currentNoteId, setCurrentNoteId] = React.useState("")
     // console.log(currentNoteId);
     const [tempNoteText, setTempNoteText] = React.useState("")
 
@@ -20,6 +21,7 @@ export default function App() {
 
     const sortedNotes = notes.sort((a, b) => b.updatedAt - a.updatedAt)
 
+    //useEffect()iki argüman alır: bir yan etki fonksiyonu ve bir dizi bağımlılığı.
     React.useEffect(()=>{
         const unsubscribe = onSnapshot(notesCollection, function(snapshot) {
             const notesArr = snapshot.docs.map(doc => ({
@@ -27,24 +29,19 @@ export default function App() {
                 id: doc.id
             }))
             setNotes(notesArr)
-            // console.log("THINGS ARE CHANGING!")
         })
         return unsubscribe;
-
-        // localStorage.setItem("notes", JSON.stringify(notes))
-        //useEffect notlar dizisini yerel depolamaya kaydetmek için React Hooks u kullanıyor
-        //? console.log(JSON.stringify(notes[0].body.split("\n")))
-    },[]) // fonksiyonun bağlı olduğu değişken notes-dur
+    },[])
     
-    React.useEffect(() => {
+    React.useEffect(() => { 
         if (!currentNoteId) {
-            setCurrentNoteId(notes[0]?.id)
+            setCurrentNoteId(notes[0]?.id) // dizi değiştiğinde çağrılır. ilk diziye atanır
         }
     }, [notes])
 
     React.useEffect(() => {
         if (currentNote) {
-            setTempNoteText(currentNote.body)
+            setTempNoteText(currentNote.body) //  Bu kod, tempNoteTextdeğişkeni geçerli notun gövdesine ayarlar.
         }
     }, [currentNote])
 
@@ -65,47 +62,13 @@ export default function App() {
         }
         const newNoteRef = await addDoc(notesCollection, newNote)
         setCurrentNoteId(newNoteRef.id)
-    
-
-        // const newNote = {
-        //     id: nanoid(),
-        //     body: "# Type your markdown note's title here"
-        // }
-        // setNotes(prevNotes => [newNote, ...prevNotes]) // yeni not, notlar dizesine eklenir
-        // setCurrentNoteId(newNote.id) // yeni notun kimliği güncellenir.
     }
     
     async function updateNote(text) {
-
         const docRef = doc(db, "notes", currentNoteId)
         await setDoc(docRef, { body: text, updatedAt: Date.now() }, { merge: true })
-
-       // Güncellenen dizi en üste geçer
-        // setNotes(oldNotes => {
-        //     const newArray = []
-        //     for(let i = 0; i < oldNotes.length; i++) {
-        //         const oldNote = oldNotes[i]
-        //         if(oldNote.id === currentNoteId) {
-        //             newArray.unshift({ ...oldNote, body: text })
-        //         } else {
-        //             newArray.push(oldNote)
-        //         }
-        //     }
-        //     return newArray
-        // })
-        
-        // id'ye göre sıralanır
-        // setNotes(oldNotes => oldNotes.map(oldNote => {
-        //*  return oldNote.id === currentNoteId ? { ...oldNote, body: text } : oldNote
     }
-    
-    // function deleteNote(event,noteId) {
-    //     event.stopPropagation()
-    //     setNotes(oldNotes => oldNotes.filter(note => note.id !== noteId))
-    //     // console.log("deleted note", noteId)
-    //     //belirtilen kimliğe sahip notu kaldırmak için notlar dizisini filtreler.
-    //     //İşlev döndürürse true öğe yeni notlar dizisine dahil edilir.
-    // }
+
     async function deleteNote(noteId) {
         const docRef = doc(db, "notes", noteId)
         await deleteDoc(docRef)
